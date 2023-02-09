534611d701c379243a8116d6ff22153c9fa1110a
xiumubai <1547702880@qq.com>
2023-02-09 22:00:07 +0800

    feat: 购物车样式

--- pages/cart/cart.js
+++ pages/cart/cart.js
@@ -1,11 +1,23 @@
-Component({
-  pageLifetimes: {
-    show() {
-      if (typeof this.getTabBar === 'function' && this.getTabBar()) {
-        this.getTabBar().setData({
-          selected: 2,
-        });
-      }
-    },
+Page({
+  onShow() {
+    if (typeof this.getTabBar === 'function' && this.getTabBar()) {
+      this.getTabBar().setData({
+        selected: 2,
+      });
+    }
+  },
+
+  data: {
+    checked: true,
+    price: 0,
+  },
+
+  onChange(event) {
+    this.setData({
+      checked: event.detail,
+    });
+  },
+  onChangePrice(event) {
+    console.log(event.detail);
   },
 });
