cfbfdba2d67588f11de3f32918a0d788e62e95ec
xiumubai <1547702880@qq.com>
2023-02-13 16:09:39 +0800

    feat: 添加时间选择器

--- pages/order/detail/index.js
+++ pages/order/detail/index.js
@@ -1,6 +1,6 @@
 // pages/order/index.js
-import { buy, trade } from '../../../utils/api';
-
+import { buy, trade, findUserAddress } from '../../../utils/api';
+import { formatDate } from '../../../utils/index';
 Page({
   /**
    * 页面的初始数据
@@ -8,12 +8,30 @@ Page({
   data: {
     list: [],
     totoalPrice: 0,
+    address: {},
+    show: false,
+    // 用户是否有默认地址
+    hasAddress: false,
+    // 购买人名称
+    buyName: '',
+    // buyPhone
+    buyPhone: null,
+    // 备注
+    remarks: '',
+    // 送货地址id
+    userAddressId: '',
+    // 期望送达日期
+    deliveryDate: '请选择配送时间',
+    minDate: new Date().getTime(),
+    currentDate: new Date().getTime(),
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow() {},
+  onShow() {
+    this.getUserDefaultAddress();
+  },
 
   /**
    * 生命周期函数--监听页面加载
@@ -28,15 +46,68 @@ Page({
   },
 
   /**
+   * 事件：展示时间选择器
+   */
+  onShowDateTimerPopUp() {
+    this.setData({ show: true });
+  },
+
+  /**
+   * 事件：关闭时间选择器
+   */
+  onClose() {
+    this.setData({ show: false });
+  },
+
+  /**
+   * 事件：修改时间
+   */
+  onChangeDateTime(event) {
+    console.log(event);
+    this.setData({
+      currentDate: event.detail,
+    });
+  },
+
+  /**
+   * 事件：timerpicker点击确定
+   */
+  onConfirmTimerPicker(event) {
+    const time = formatDate(new Date(event.detail), 'YYYY-MM-DD');
+    this.setData({ show: false, deliveryDate: time });
+  },
+
+  /**
+   * 事件：timerpicker点击取消
+   */
+  onCancelTimePicker() {
+    this.setData({ show: false });
+  },
+
+  /**
    * 事件：去结算
    */
-  handleGoToPay() {},
+  handleGoToPay() {
+    const { buyName, buyPhone, deliveryDate, remarks, userAddressId, list } =
+      this.data;
+    const params = {
+      buyName,
+      buyPhone,
+      deliveryDate,
+      remarks,
+      userAddressId,
+      cartList: list,
+    };
+
+    // 支付成功，跳转到支付成功页面
+    console.log(params);
+  },
   /**
    * 事件：修改商品数量
    */
   onChangeCount(event) {
-    // TODO:手动输入的数量，无法判断是增加还是减少
-    // 手动输入商品数量以后如何计算商品的总价
+    // 手动输入的数量，无法判断是增加还是减少
+    // 手动输入商品数量以后如何计算商品的总价，完善中，敬请期待。。。
     const newCount = event.detail;
     const goodsId = event.target.dataset.goodsid;
     console.log(event);
@@ -78,7 +149,6 @@ Page({
   async getBuyGoodsDetail(options) {
     const res = await buy(options);
     if (res.code === 200) {
-      console.log(res);
       this.setData({
         list: res.data.cartVoList,
         totoalPrice: res.data.totalAmount,
@@ -96,6 +166,23 @@ Page({
         list: res.data.cartVoList,
         totoalPrice: res.data.totalAmount,
       });
+    }
+  },
+
+  /**
+   * 获取用户默认地址
+   */
+  async getUserDefaultAddress() {
+    const res = await findUserAddress();
+    if (res.code === 200 && res.data.length > 0) {
+      res.code.forEach((item) => {
+        if (item.isDefault) {
+          // 选择默认地址作为收货人的地址信息
+          this.setData({ address: item, hasAddress: true });
+        }
+      });
+    } else {
+      this.setData({ address: {}, hasAddress: false });
     }
   },
 });
---
+++ utils/index.js
@@ -0,0 +1,47 @@
+const formatTime = (date) => {
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  const hour = date.getHours();
+  const minute = date.getMinutes();
+  const second = date.getSeconds();
+
+  return `${[year, month, day].map(formatNumber).join('/')} ${[
+    hour,
+    minute,
+    second,
+  ]
+    .map(formatNumber)
+    .join(':')}`;
+};
+
+const formatNumber = (n) => {
+  n = n.toString();
+  return n[1] ? n : `0${n}`;
+};
+
+export function formatDate(date, formatstr) {
+  var arrweek = ['日', '一', '二', '三', '四', '五', '六'];
+  var str = formatstr
+    .replace(/yyyy|YYYY/, date.getFullYear())
+    .replace(/yy|YY/, $addZero(date.getFullYear() % 100, 2))
+    .replace(/mm|MM/, $addZero(date.getMonth() + 1, 2))
+    .replace(/m|M/g, date.getMonth() + 1)
+    .replace(/dd|DD/, $addZero(date.getDate(), 2))
+    .replace(/d|D/g, date.getDate())
+    .replace(/hh|HH/, $addZero(date.getHours(), 2))
+    .replace(/h|H/g, date.getHours())
+    .replace(/ii|II/, $addZero(date.getMinutes(), 2))
+    .replace(/i|I/g, date.getMinutes())
+    .replace(/ss|SS/, $addZero(date.getSeconds(), 2))
+    .replace(/s|S/g, date.getSeconds())
+    .replace(/w|g/, $addZero(date.getDay(), 2))
+    .replace(/W/g, arrweek[date.getDay()]);
+  return str;
+}
+function $addZero(v, size) {
+  for (var i = 0, len = size - (v + '').length; i < len; i++) {
+    v = '0' + v;
+  }
+  return v + '';
+}
