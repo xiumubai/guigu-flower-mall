277a8ca5568884e91e8beef5e388cbc7b465c50b
xiumubai <1547702880@qq.com>
2023-02-16 20:02:49 +0800

    feat: 订单详情

--- pages/order/detail/index.js
+++ pages/order/detail/index.js
@@ -1,66 +1,63 @@
 // pages/order/detail/index.js
+import { orderDetail } from '../../../utils/api';
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-
+    order: {},
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-
+    console.log(options);
+    this.getOrderDetail(options.orderNo);
   },
 
   /**
+   * 获取订单详情
+   */
+  async getOrderDetail(orderNo) {
+    const res = await orderDetail(orderNo);
+    console.log(res);
+    if (res.code === 200) {
+      this.setData({ order: res.data });
+    }
+  },
+  /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady() {
-
-  },
+  onReady() {},
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow() {
-
-  },
+  onShow() {},
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide() {
-
-  },
+  onHide() {},
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload() {
-
-  },
+  onUnload() {},
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh() {
-
-  },
+  onPullDownRefresh() {},
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom() {
-
-  },
+  onReachBottom() {},
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage() {
-
-  }
-})
+  onShareAppMessage() {},
+});
